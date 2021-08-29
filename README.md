77f29f3060c2e2d681438d9840525c113f21bf08
선한기획자 박현준 <imgoodplanner@gmail.com>
2021-08-29 22:05:40 +0900

    Stack and Tab nav

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # What is
 - Copy and Run. Not edit yet !
 
+# etc
+- expo iso simulater reload menu : Cmd + D
 
 # Status
 - [ ] Apollo Login
--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,13 +1,79 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Feed from "../screens/Feed";
+import { View } from "react-native";
+import TabIcon from "../components/nav/TabIcon";
+import SharedStackNav from "./SharedStackNav";
 
 const Tabs = createBottomTabNavigator();
 
 export default function LoggedInNav() {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Feed" component={Feed} />
+    <Tabs.Navigator
+      // tabBarOptions={{
+      // activeTintColor: "white",
+      // style: {
+      //   borderTopColor: "rgba(255, 255, 255, 0.3)",
+      //   backgroundColor: "black",
+      // },
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        showLabel: false,
+        tabBarStyle: {
+          borderTopColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+          elevation: 0, shadowOpacity: 0
+        }
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"home"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Feed" />}
+      </Tabs.Screen>
+      <Tabs.Screen
+        name="Search"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"search"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Search" />}
+      </Tabs.Screen>
+      <Tabs.Screen
+        name="Camera"
+        component={View}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"camera"} color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Notifications"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"heart"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Notifications" />}
+      </Tabs.Screen>
+      <Tabs.Screen
+        name="Me"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"person"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Me" />}
+      </Tabs.Screen>
     </Tabs.Navigator>
   );
 }
